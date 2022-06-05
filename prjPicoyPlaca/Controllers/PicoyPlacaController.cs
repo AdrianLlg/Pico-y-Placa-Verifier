59018c59bd7c7c4936c51da15d855221a64f3cf4
Adrián Llangarí <adrianllangari@gmail.com>
2022-06-05 16:57:24 -0500

    Add project files.

---
+++ prjPicoyPlaca/Controllers/PicoyPlacaController.cs
@@ -0,0 +1,63 @@
+﻿using prjPicoyPlaca.Models;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace prjPicoyPlaca.Controllers
+{
+    public class PicoyPlacaController : Controller
+    {
+        // GET: PicoyPlaca
+        public ActionResult PicoyPlacaVerifier()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult PicoyPlacaVerifier(InputsPicoyPlaca inputsPicoYPlaca)
+        {
+            try
+            {
+                if (InputsVerifier(inputsPicoYPlaca.plateNumber, inputsPicoYPlaca.date))
+                {
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    ViewBag.Message = "Please, provide a valid Date.";
+                    return View();
+                }
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+
+        public bool InputsVerifier(string plateNumber, string date)
+        {
+            try
+            {
+                DateTime dayPicked = DateTime.ParseExact(date, "yyyy-MM-dd", CultureInfo.InvariantCulture);
+                DateTime today = DateTime.Today;
+
+                if (dayPicked.Date >= today)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ prjPicoyPlaca/Models/InputsPicoyPlaca.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace prjPicoyPlaca.Models
+{
+    public class InputsPicoyPlaca
+    {
+        [Display(Name = "Plate Number:", AutoGenerateFilter = false)]
+        [Required(ErrorMessage = "Please, fill the information required.")]
+        [StringLength(4, MinimumLength = 3, ErrorMessage = "Please, provide a valid Plate Number.")]
+        [RegularExpression(@"^[0-9]{0,9}$", ErrorMessage = "The entry must be numeric characters only.")]
+        public string plateNumber { get; set; }
+
+        [Display(Name = "Date:", AutoGenerateFilter = false)]
+        [Required(ErrorMessage = "Please, fill out the information required.")]
+        public string date { get; set; }
+
+        [Display(Name = "Time:", AutoGenerateFilter = false)]
+        [Required(ErrorMessage = "Please, fill the information required.")]
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:HH:mm}")]
+        [DataType(DataType.Time)]
+        public TimeSpan time { get; set; }
+    }
+}
